393a119219e06a73dfecdc78477a549d30da46da
gersonhiggins <gahg.10gerson@gmail.com>
2023-06-20 18:16:11 -0400

    Add Actions, Update styles, Check linters

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -5,10 +5,15 @@ import './Profile.scss';
 const Profile = () => {
   const joinedMissions = useSelector((state) => state.missions.joinedMissions);
   const allMissions = useSelector((state) => state.missions.missions);
+  const addedRockets = useSelector((state) => state.rocket.addedRockets);
+  const allRockets = useSelector((state) => state.rocket.rockets);
 
   const joinedMissionNames = allMissions
     .filter((mission) => joinedMissions.includes(mission.mission_id))
     .map((mission) => mission.mission_name);
+  const addedRocketsNames = allRockets
+    .filter((rocket) => addedRockets.includes(rocket.id))
+    .map((rocket) => rocket.name);
 
   return (
     <div className="profile">
@@ -22,7 +27,11 @@ const Profile = () => {
       </div>
       <div className="booked-rockets">
         <h2>My Rockets</h2>
-        <ul>{/* ROCKKEEEEET */}</ul>
+        <ul>
+          {addedRocketsNames.map((rocketName) => (
+            <li key={rocketName}>{rocketName}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/views/Rockets/Rockets.jsx
+++ src/views/Rockets/Rockets.jsx
@@ -1,17 +1,45 @@
 import React, { useEffect } from 'react';
+import { Vortex } from 'react-loader-spinner';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets } from '../../redux/rockects/rocketSlice';
+import {
+  setAddedRockets,
+  addRocket,
+  fetchRockets,
+  removeRocket,
+} from '../../redux/rockects/rocketSlice';
 import './Rockets.scss';
 
 const Rockets = () => {
   const rocketList = useSelector((state) => state.rocket);
+  const addedRockets = useSelector((state) => state.rocket.addedRockets);
   const dispatch = useDispatch();
+  const handleSubmit = (rocketId) => {
+    dispatch(addRocket(rocketId));
+  };
+  const handleRemove = (rocketId) => {
+    dispatch(removeRocket(rocketId));
+  };
   useEffect(() => {
-    dispatch(fetchRockets());
-  }, [dispatch]);
+    if (!addedRockets.length) {
+      dispatch(fetchRockets());
+      dispatch(setAddedRockets(rocketList.rockets));
+    }
+  }, [addedRockets.length, dispatch, rocketList.rockets]);
   return (
     <div className="rocket-div">
-      {rocketList.loading && <div> ... Loading</div>}
+      {rocketList.loading && (
+      <div className="loading-wrapper">
+        <Vortex
+          visible
+          height="80"
+          width="80"
+          ariaLabel="vortex-loading"
+          wrapperStyle={{}}
+          wrapperClass="vortex-wrapper"
+          colors={['blue', 'purple']}
+        />
+      </div>
+      )}
       {!rocketList.loading && rocketList.error ? (
         <div>
           Error:
@@ -20,22 +48,40 @@ const Rockets = () => {
       ) : null}
       {!rocketList.loading && rocketList.rockets.length ? (
         <ul className="rocket-list">
-          {
-            rocketList.rockets.map((rock) => (
-              <li className="rocket" key={rock.id}>
-                <img src={rock.flickr_images[0]} alt="rocket" width="200px" />
-                <div className="info">
-                  <p>{rock.name}</p>
-                  <p>{rock.description}</p>
-                  <button className="reserve-btn" type="button">Reserve Rocket</button>
-                </div>
-              </li>
-            ))
-          }
+          {rocketList.rockets.map((rocket) => (
+            <li className="rocket" key={rocket.id}>
+              <img src={rocket.flickr_images[0]} alt="rocket" width="200px" />
+              <div className="info">
+                <p>{rocket.name}</p>
+                {addedRockets.includes(rocket.id) ? (
+                  <p>
+                    <span>Reserved </span>
+                    {rocket.description}
+                  </p>
+                ) : <p>{rocket.description}</p>}
+                {addedRockets.includes(rocket.id) ? (
+                  <button
+                    className="cancel-btn"
+                    type="button"
+                    onClick={() => handleRemove(rocket.id)}
+                  >
+                    Cancel Reservation
+                  </button>
+                ) : (
+                  <button
+                    className="reserve-btn"
+                    type="button"
+                    onClick={() => handleSubmit(rocket.id)}
+                  >
+                    Reserve Rocket
+                  </button>
+                )}
+              </div>
+            </li>
+          ))}
         </ul>
       ) : null}
     </div>
   );
 };
-
 export default Rockets;
